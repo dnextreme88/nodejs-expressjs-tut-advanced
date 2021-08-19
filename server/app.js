e37afc1e3e47ccf6ba0d96d6dbf09988f454b703
dnextreme88 <storm_iz_here@yahoo.com>
2021-08-19 08:40:25 +0800

    Inspecting the session object

--- server/app.js
+++ server/app.js
@@ -39,7 +39,6 @@ module.exports = (config) => {
 
   app.use(async (req, res, next) => {
     try {
-      req.session.visits = req.session.visits ? req.session.visits + 1 : 1;
       const names = await speakers.getNames();
       res.locals.speakerNames = names;
       return next();
